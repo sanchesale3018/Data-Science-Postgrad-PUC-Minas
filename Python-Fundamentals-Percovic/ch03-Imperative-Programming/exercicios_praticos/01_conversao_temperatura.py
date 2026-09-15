9e0973c36c8c78d0678330e42333cb805a364a74
Alexandre dos Santos Sanches <sanches.ale.3018@gmail.com>
2026-09-15 09:46:18 -0300

    Implementando funções unput, print e eval

--- Python-Fundamentals-Percovic/ch03-Imperative-Programming/exercicios_praticos/01_conversao_temperatura.py
+++ Python-Fundamentals-Percovic/ch03-Imperative-Programming/exercicios_praticos/01_conversao_temperatura.py
@@ -7,4 +7,4 @@ fahrenheit = eval(input('Digite a temperatura fahrenheit: '))
 
 celsius = (5*(fahrenheit-32))/9
 
-print(f'A temperatura fahrenheit é: {celsius}' )
+print(f'A temperatura Celsius é: {celsius}' )
