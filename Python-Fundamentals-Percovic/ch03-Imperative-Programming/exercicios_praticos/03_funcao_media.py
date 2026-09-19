3579db7b520fa8c2e641bec286538c188d37b7c7
Alexandre dos Santos Sanches <sanches.ale.3018@gmail.com>
2026-09-18 21:40:11 -0300

    exercício: função media aritmética

---
+++ Python-Fundamentals-Percovic/ch03-Imperative-Programming/exercicios_praticos/03_funcao_media.py
@@ -0,0 +1,12 @@
+
+def media(valor1,valor2):
+    return (valor1 + valor2)/2
+
+print("Vamor calcular a média aritmética de dois números")
+
+valor1 = int(input("Digite o 1°valor: "))
+valor2 = int(input("Digite o 2°valor: "))
+
+media = media(valor1, valor2)
+
+print(f"A média entre os dois números é: {media}")
